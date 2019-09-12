b21dc7c2e72315b782242e23682b413a2efac687
ocean <dumpinfo@163.com>
2019-09-13 03:22:38 +0800

    first commit

---
+++ InteractiveComputerGraphics/TUTORS/mk.sh
@@ -0,0 +1,7 @@
+g++ glm.c fog.c -o fog -lGLU -lGL -lglut
+g++ glm.c lightmaterial.c -o lightmaterial -lGLU -lGL -lglut
+g++ glm.c lightposition.c -o lightposition -lGLU -lGL -lglut
+g++ glm.c projection.c -o projection -lGLU -lGL -lglut
+g++ glm.c shapes.c -o shapes -lGLU -lGL -lglut
+g++ glm.c texture.c -o texture -lGLU -lGL -lglut
+g++ glm.c transformation.c -o transformation -lGLU -lGL -lglut
